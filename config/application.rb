021139e09c4abb2bd1767b2a0358729bbdfc8dee
Dave Nevogt <dnevogt@gmail.com>
2013-04-24 09:47:47 -0400

    adding devise and some bootstrap

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Davetest
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # this was required for running devise on heroku 3.1+
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Davetest::Application.routes.draw do
+  devise_for :users
+
   get "pages/about"
 
   root :to => 'pages#home'
